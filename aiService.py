3063bbc1be4519251d50c6366e93d74ec134f2be
Ajay Tripathy <aurotripathy@auro-tripathys-MacBook-Pro.local>
2012-05-29 19:20:06 -0700

    temporary service

--- aiService.py
+++ aiService.py
@@ -7,20 +7,22 @@ availableRooms = set()
 def generateRoom():
   room = uuid.uuid1()
   availableRooms.add(room)
+  return str(room.int)
 
 def joinRoom():
   room = availableRooms.pop()
+  return room
     
 class ConnectObj(object):
   def connect(self, name, callback):
     print("haro "+ name )
     if len(availableRooms) > 0:
-      joinRoom()
-      callback("joined")
+      room = joinRoom()
+      callback({'type': "joined", 'channel' : room})
     else:
-      generateRoom()	
-      callback("created")
-
+      room = generateRoom()	
+      callback({'type': "created", 'channel': room})
+  
 
 bridge = Bridge(api_key= 'cb0ec2c9')
 bridge.publish_service('connect', ConnectObj())
