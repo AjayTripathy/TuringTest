5578f071490380fdb17cf372ba7f8695471b4c3a
Ajay Tripathy <aurotripathy@auro-tripathys-MacBook-Pro.local>
2012-05-29 22:11:03 -0700

    finished game logic

--- aiService.py
+++ aiService.py
@@ -1,8 +1,10 @@
+import random
 import cleverbot
 import uuid
 from flotype.bridge import Bridge 
 
 availableRooms = set() 
+aiRooms = {}
 
 def generateRoom():
   room = uuid.uuid1()
@@ -11,17 +13,50 @@ def generateRoom():
 
 def joinRoom():
   room = availableRooms.pop()
-  return room
+  return str(room.int)
+
+class CleverBotChat(object):
+  def message(self, channel, sender, message, human):
+    #tell cleverbot
+    print 'message received'
+    print channel
+    if (human):
+      msg = aiRooms[channel].Ask(message)
+      chan = bridge.get_channel(channel)
+      chan.message(channel, "Stranger", msg, False)
+  def endGame(self,conditions):
+    pass
     
 class ConnectObj(object):
   def connect(self, name, callback):
     print("haro "+ name )
     if len(availableRooms) > 0:
-      room = joinRoom()
-      callback({'type': "joined", 'channel' : room})
+      rand = random.randint(0,1)
+      #rand = 1
+      if (rand == 0):
+        print "human player"
+        room = joinRoom()
+        callback({'type': "joined", 'channel' : room, 'role': 'tricker' })
+        chan = bridge.get_channel(room)
+        chan.startGame()
+      else:
+        print 'hopefully letting cleverbot in'
+        room1 = joinRoom()
+        aiRooms[room1] = cleverbot.Session()
+        bridge.join_channel(room1, CleverBotChat())
+        room2 = generateRoom()
+        callback({'type': "created", 'channel': room2, 'role': 'guesser'})
     else:
       room = generateRoom()	
-      callback({'type': "created", 'channel': room})
+      callback({'type': "created", 'channel': room, 'role': 'guesser'})
+  def guessRobot(self, isRobot, channel):
+      chan = bridge.get_channel(channel)
+      if ( (channel in aiRooms) and (isRobot) ):
+          chan.endGame({'guess': True})
+      elif ( (not (channel in aiRooms) ) and (not isRobot) ):
+          chan.endGame({'guess': True})
+      else:
+          chan.endGame({'guess': False})
   
 
 bridge = Bridge(api_key= 'cb0ec2c9')
